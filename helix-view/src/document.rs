cca4c8f5ad4886b78ea0ad1b5604acced71369e2
gavlig <gavlig@gmail.com>
2023-12-03 13:11:11 +0100

    SCRATCH_BUFFER_NAME changed: "[scratch]" -> "scratch"

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -45,7 +45,7 @@ const DEFAULT_INDENT: IndentStyle = IndentStyle::Tabs;
 
 pub const DEFAULT_LANGUAGE_NAME: &str = "text";
 
-pub const SCRATCH_BUFFER_NAME: &str = "[scratch]";
+pub const SCRATCH_BUFFER_NAME: &str = "scratch";
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Mode {
